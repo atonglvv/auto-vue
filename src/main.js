16f7b157cd3a8395937e2f868f5914cd1dc9f7da
Innovation <lt1550919167@gmail.com>
2018-11-30 18:48:41 +0800

    error

--- src/main.js
+++ src/main.js
@@ -1,11 +1,45 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+// import Vue from 'vue'
+// import App from './App'
+// import router from './router'
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+//
+// Vue.use(ElementUI)
+//
+// Vue.config.productionTip = false
+//
+// /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   router,
+//   components: { App },
+//   template: '<App/>'
+// })
+
+
+// import Vue from 'vue';
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
+// import App from './App.vue';
+//
+// Vue.use(ElementUI);
+//
+// new Vue({
+//   el: '#app',
+//   render: h => h(App)
+// });
+
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
-
+Vue.use(ElementUI);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
---
+++ src/page/login/login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <input type="text" name="" value="" placeholder="用户名">
+    <router-view/>
+    <h1>Hello Vue</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
