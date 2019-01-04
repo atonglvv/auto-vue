2f658e417a35c0dde15e26b609bfe3d33419136a
mrdpp111 <44170826+mrdpp111@users.noreply.github.com>
2019-01-04 21:01:06 +0800

    personal

--- src/page/personal/personal.vue
+++ src/page/personal/personal.vue
@@ -6,14 +6,65 @@
       </div>
       <div class="top-right">
         <div class="t-r-t">吕小小英雄</div>
-        <div class="t-r-m"><i class="el-icon-edit"></i>修改资料</div>
+        <div class="t-r-m"><i class="el-icon-edit-outline"></i>修改资料</div>
       </div>
     </div>
+
     <div class="personal">
-      <el-tabs :tab-position="tabPosition" style="height: 200px;">
-        <el-tab-pane label=">>我的评论">我的评论</el-tab-pane>
-        <el-tab-pane label=">>我的收藏">我的收藏</el-tab-pane>
-        <el-tab-pane label=">>账户设置">账户设置</el-tab-pane>
+      <el-tabs :tab-position="tabPosition" style="height: 500px;">
+        <el-tab-pane label=">>我的评论">
+          <div class="personal-i">我的评论</br>
+          </div><hr>
+          <div class="personal-title">
+            《这是标题》
+          </div>
+          <div class="personal-content">
+            评论日期：2019-1-1
+            <el-button type="primary" round>查看</el-button>
+            <el-button type="danger" icon="el-icon-delete" circle></el-button>
+            <hr/>
+          </div>
+        </el-tab-pane>
+
+
+
+        <el-tab-pane label=">>我的收藏">
+          <div class="personal-i">
+            我的收藏</br>
+          </div><hr>
+          <div class="personal-title">
+            《这是标题》
+          </div>
+          <div class="personal-content">
+            收藏日期：2019-1-1
+            <el-button type="primary" round>查看</el-button>
+            <el-button type="danger" icon="el-icon-delete" circle></el-button>
+            <hr/>
+          </div>
+        </el-tab-pane>
+
+
+
+        <el-tab-pane label=">>账户信息">
+          <div class="personal-i">账户信息</br>
+          </div>
+          <hr>
+          <div class="img">
+            <img src="../../assets/images/tx.jpg" alt="">
+          </div>
+          <div class="xx">
+            <ul>
+            <li>吕小小英雄</li><br/><br/>
+            <li>性别：男</li><br/><br/>
+            <li><i class="el-icon-date"></i>注册：2019-1-1</li><br/><br/>
+
+            <li><i class="el-icon-mobile-phone"></i>电话：123</li><br/><br/>
+            <li></li><br/><br/>
+            <li></li><br/><br/>
+            <li></li><br/><br/>
+            </ul>
+          </div>
+        </el-tab-pane>
       </el-tabs>
     </div>
   </div>
@@ -63,8 +114,48 @@ export default {
   font-size: 15px;
   text-align: center;
 }
+
 .personal{
   margin-top: 30px;
   margin-left: 150px;
+  height: 600px;
+}
+.personal hr{
+  margin-top:25px;
+  border:1px solid gray;
+}
+.personal-i{
+  margin-left:100px;
+  font-size: 20px;
+  color: red;
+}
+.personal-content hr{
+  border:1px dashed gray;
+}
+
+.personal-content{
+  margin-top: 45px;
+  margin-left: 100px;
+  font-size: 18px;
+}
+.personal-title{
+  margin-left:100px;
+  margin-top: 20px;
+  font-size: 15px;
+  color: red;
+}
+.img{
+  float: left;
+  margin-left: 80px;
+  margin-top: 40px;
+  width: 200px;
+  height: 200px;
+
+}
+.xx{
+  float: left;
+  margin-top: 40px;
+  margin-left: 30px;
+  font-size: 20px;
 }
 </style>
